4800247821f8529528a5e5c0eebbcab90962bb21
Valery Krylov <Valery Krylov@DESKTOP-36T2PRS>
2024-05-03 12:45:43 +0300

    Добавьте файлы проекта.

---
+++ Diplom/CangeWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Diplom
+{
+    /// <summary>
+    /// Логика взаимодействия для CangeWindow.xaml
+    /// </summary>
+    public partial class CangeWindow : Window
+    {
+        public CangeWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Diplom/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+using System.Windows;
+
+namespace Diplom
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuCloseApp_Click(object sender, RoutedEventArgs e) => Close();
+
+        private void MenuHelpEdit_Click(object sender, RoutedEventArgs e)
+        {
+            UserHelpWindow helpWindow = new UserHelpWindow();
+            helpWindow.ShowDialog();
+        }
+
+        private void MenuChange_Click(object sender, RoutedEventArgs e)
+        {
+            CangeWindow changeWindow = new CangeWindow();
+            changeWindow.ShowDialog();
+        }
+    }
+}
